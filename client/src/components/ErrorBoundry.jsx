18cc411a9b8756cb89f68050254cc813ff4c935b
AngelStch <adstoichev@gmail.com>
2023-12-18 23:25:21 +0200

    removed logs

--- client/src/components/ErrorBoundry.jsx
+++ client/src/components/ErrorBoundry.jsx
@@ -10,14 +10,12 @@ export default class ErrorBoundary extends Component {
     }
     
     static getDerivedStateFromError(err) {
-        console.log('GetDerivedStateFromError');
         return {
             hasError: true,
         }
     }
 
     componentDidCatch(error, errorInfo) {
-        console.log('componentDidCatch')
     }
     render() {
         if (this.state.hasError) {
